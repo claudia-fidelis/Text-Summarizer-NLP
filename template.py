b87b22786d068999043cd6cf657b20c69706a7d9
Claudia <claudiafidelis17@gmail.com>
2023-09-19 19:41:04 -0300

    files add

--- template.py
+++ template.py
@@ -17,7 +17,7 @@ list_of_files = [
     f"src/{project_name}/pipeline/__init__.py",
     f"src/{project_name}/entity/__init__.py",
     f"src/{project_name}/constants/__init__.py",
-    "consig/consig.yaml",
+    "config/config.yaml",
     "params.yaml",
     "app.py",
     "main.py",
@@ -25,7 +25,6 @@ list_of_files = [
     "requirements.txt",
     "setup.py",
     "research/trials.ipynb",
-    "test.py",
 ]
 
 for filepath in list_of_files:
